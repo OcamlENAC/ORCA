f1015d90d7a0c68d2d3a4b93aa0c4330ef956209
ChaangD <chang.delair@gmail.com>
2018-12-11 15:28:31 +0100

    Delete ia.ml

--- ia.ml
+++
@@ -1,4 +0,0 @@
-let update (obj : Object.obj) dt =
-	(** Retourne le nouveau point avec les vitesses résultantes de l'algo ORCA*)
-	(* Object.set_obj_pos obj {Geometry.x = ((obj.position).x +. 2.) ; Geometry.y = ((obj.position).x +. 2.)}  *)
-	Object.update_pos obj dt
