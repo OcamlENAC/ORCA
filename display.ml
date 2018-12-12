b176549add1b9bfc5a89bfc835311b81485e0f56
ChaangD <chang.delair@gmail.com>
2018-12-12 15:00:13 +0100

    objets se deplacent vers dest

--- display.ml
+++ display.ml
@@ -1,6 +1,20 @@
+
+(* ocamlmktop -o orca graphics.cma unix.cma geometry.ml object.ml orca.ml display.ml 
+ *)
+
+
+(*
 let update objects refreshing_time =
 	(** Met a jour la liste de objects en appliquant l'ORCA *)
 	List.map (fun o -> Orca.update o refreshing_time) objects
+*)
+
+
+let update objects refreshing_time =
+	(** Met a jour la liste de objects en appliquant l'ORCA *)
+	(* Array.map (fun o -> Orca.update o refreshing_time) objects *)
+	Orca.update objects refreshing_time
+
 
 let render_obj (o : Object.obj) = 
 	(** Afficher un object *)
@@ -10,21 +24,20 @@ let render_obj (o : Object.obj) =
 
 let render objects =
 	(** Affiche tous les objects et les renvois*)
-	List.map render_obj objects
+	Array.map render_obj objects
 
 
 
 
-let rec start_animation objects =
+let rec start_animation objects refreshing_time =
 	(** Boucle d'animation, ne renvoit rien*)
-	let refreshing_time = 1. /. 25. in
 	let no = update objects refreshing_time in
 	let new_objects = render no in
 	begin
 		Unix.sleepf (refreshing_time);
 		Graphics.set_color Graphics.white;
 		Graphics.fill_rect 0 0 (Graphics.size_x ()) (Graphics.size_y ()) ;
-		start_animation new_objects;
+		start_animation new_objects refreshing_time;
 	end
 
 
@@ -33,5 +46,9 @@ let () =
 	Graphics.open_graph " 800x600";
 	let p2 = Object.init_from_pos 150. 200.
 	and p1 = Object.init_from_pos 500. 10. in
-	let objects = p1 :: p2 :: [] in
-	start_animation objects
+	let objects = [|p1 ; p2|]  (* p1 :: p2 :: [] in *)
+	and refreshing_time = 1. /. 25. in
+	start_animation objects refreshing_time
+
+
+
