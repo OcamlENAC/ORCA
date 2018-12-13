2d6a9748541cb353187ae9fbb75281d2117634d4
ChaangD <chang.delair@gmail.com>
2018-12-13 18:01:45 +0100

    affichage du cone

--- display.ml
+++ display.ml
@@ -16,27 +16,27 @@ let update objects refreshing_time =
 	Orca.update objects refreshing_time
 
 
-let render_obj (o : Object.obj) = 
+let render_obj (o : Object.obj) refreshing_time = 
 	(** Afficher un object *)
 	Graphics.set_color Graphics.black;
-	Graphics.fill_circle (int_of_float (o.position).x) (int_of_float (o.position).y) (int_of_float o.diameter);
+	Graphics.draw_circle (int_of_float (o.position).x) (int_of_float (o.position).y) (int_of_float (o.diameter /. 2.));
+	Graphics.set_color Graphics.blue;
+	Graphics.moveto (int_of_float (o.position).x) (int_of_float (o.position).y);
+	Graphics.lineto (int_of_float (o.position.x +. (o.speed.x *. 2.))) (int_of_float (o.position.y +. (o.speed.y *. 2.)));
 	o
 
-let render objects =
+let render objects refreshing_time =
 	(** Affiche tous les objects et les renvois*)
-	Array.map render_obj objects
-
-
-
+	Array.map (fun t -> render_obj t refreshing_time) objects 
 
 let rec start_animation objects refreshing_time =
 	(** Boucle d'animation, ne renvoit rien*)
-	let no = update objects refreshing_time in
-	let new_objects = render no in
 	begin
 		Unix.sleepf (refreshing_time);
 		Graphics.set_color Graphics.white;
 		Graphics.fill_rect 0 0 (Graphics.size_x ()) (Graphics.size_y ()) ;
+		let no = update objects refreshing_time in
+		let new_objects = render no refreshing_time in
 		start_animation new_objects refreshing_time;
 	end
 
@@ -44,11 +44,10 @@ let rec start_animation objects refreshing_time =
 let () = 
 	Printf.printf("Debut main\n");
 	Graphics.open_graph " 800x600";
-	let p2 = Object.init_from_pos 150. 200.
-	and p1 = Object.init_from_pos 500. 10. in
-	let objects = [|p1 ; p2|]  (* p1 :: p2 :: [] in *)
+	let p2 = Object.init 0. 0. 25. 25. 76. 700. 305. 50. (* Il se dirige un peu plus haut que la pos initiale de objB*)
+	and p1 = Object.init 800. 0. (-. 40.) 0. 76. 100. 295. 50. in
+	let objects = [|p1 ; p2|]  (* p1 :: p2 :: [] in  init px py sx sy d destx desty ms  
+	let sx = 5. and sy = 1. and d = 20. and destx = 0. and desty = 0. and ms = 100. *)
 	and refreshing_time = 1. /. 25. in
 	start_animation objects refreshing_time
-
-
 
