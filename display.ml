8675cd564eaffa15710eb8032614eba208fd0452
ChaangD <chang.delair@gmail.com>
2018-12-18 15:38:25 +0100

    après avoir vu le prof (open les modules)

--- display.ml
+++ display.ml
@@ -1,14 +1,13 @@
 
 (* ocamlmktop -o orca graphics.cma unix.cma geometry.ml object.ml orca.ml display.ml 
+
+	----> CA CEST MIEUX
+	-----> ocamlopt -o orca graphics.cmxa unix.cmxa geometry.ml object.ml orca.ml display.ml
  *)
 
 
-(*
-let update objects refreshing_time =
-	(** Met a jour la liste de objects en appliquant l'ORCA *)
-	List.map (fun o -> Orca.update o refreshing_time) objects
-*)
-
+open Object
+open Geometry
 
 let update objects refreshing_time =
 	(** Met a jour la liste de objects en appliquant l'ORCA *)
@@ -16,7 +15,7 @@ let update objects refreshing_time =
 	Orca.update objects refreshing_time
 
 
-let render_obj (o : Object.obj) refreshing_time = 
+let render_obj o refreshing_time = 
 	(** Afficher un object *)
 	Graphics.set_color Graphics.black;
 	Graphics.draw_circle (int_of_float (o.position).x) (int_of_float (o.position).y) (int_of_float (o.diameter /. 2.));
@@ -44,8 +43,8 @@ let rec start_animation objects refreshing_time =
 let () = 
 	Printf.printf("Debut main\n");
 	Graphics.open_graph " 800x600";
-	let p2 = Object.init 0. 0. 25. 25. 76. 700. 305. 50. (* Il se dirige un peu plus haut que la pos initiale de objB*)
-	and p1 = Object.init 800. 0. (-. 40.) 0. 76. 100. 295. 50. in
+	let p2 = Object.init 300. 200. 25. 25. 76. 600. 500. 50. (* Il se dirige un peu plus haut que la pos initiale de objB*)
+	and p1 = Object.init 500. 300. (-. 40.) 0. 76. 100. 295. 50. in
 	let objects = [|p1 ; p2|]  (* p1 :: p2 :: [] in  init px py sx sy d destx desty ms  
 	let sx = 5. and sy = 1. and d = 20. and destx = 0. and desty = 0. and ms = 100. *)
 	and refreshing_time = 1. /. 50. in
