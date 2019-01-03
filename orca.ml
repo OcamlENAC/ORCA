97487c5880c342c059d0e2353c4ac7b9085b68eb
ChaangD <chang.delair@gmail.com>
2019-01-03 15:48:00 +0100

    maj fonctionnelle avec les demi plans

--- orca.ml
+++ orca.ml
@@ -3,7 +3,7 @@ open Geometry
 
 
 let is_in_danger_zone v_relat d_cone dt = 
-	(****Verifier si la pointe du v Vopti est dans le cone****)
+	(****Verifier si la pointe du vrelative est dans le cone****)
 	(*On regarde dans l'ordre angle relatif ? -> avant le cercle ? -> après le cercle ? -> dans le cercle ?*)
 	(*Angle relatif et autres pour les conditions*) 
 	let relat_ang_vrelat_vorient = abs_float (Geometry.relative_angle v_relat d_cone.vect) in
@@ -26,7 +26,7 @@ let correct objA objB correction =
 	and new_objB = Object.set_obj_speed objB new_v_B in
 	(new_objA, new_objB)
 
-let calc_danger_cone  robot obstacle dt =
+let calc_danger_cone robot obstacle dt =
 (**** Calcul des composants du cone ****)
 	let vecttry = (Geometry.add_subst ( -. ) obstacle.position robot.position) in
 	let vect_orientation = Geometry.mult_scal (1./.dt) vecttry in
@@ -81,124 +81,86 @@ let render_cone robot obstacle d_cone in_danger dt =
 
 
 
-let get_correction vr d_cone = (*Printf.printf "%f %f  " proj_vr_cone.x proj_vr_cone.y;*)
+let get_correction vr d_cone = 
 	let r_angle = Geometry.relative_angle d_cone.vect vr in
 	let alpha = Geometry.get_angle_cone d_cone in
 	if r_angle < 0. then 
-	let proj_vr_cone = Geometry.rotate_vect vr ((-. alpha) -. r_angle) in
-	Geometry.add_subst (-.) proj_vr_cone vr
+		let proj_vr_cone = Geometry.rotate_vect vr ((-. alpha) -. r_angle) in
+		Geometry.add_subst (-.) proj_vr_cone vr
 	else 
-	let proj_vr_cone = Geometry.rotate_vect vr (alpha -. r_angle) in
-	Geometry.add_subst (-.) proj_vr_cone vr 
-
+		let proj_vr_cone = Geometry.rotate_vect vr (alpha -. r_angle) in
+	
 let calc_half_plane robot correction =
 	let vect = Geometry.add_subst ( +. ) robot.speed correction in
 	let orig = Geometry.add_subst ( +. ) robot.position vect in
 	{Geometry.origin = orig ; Geometry.vect_normal = correction ;}
 
-
 let update objects refreshing_time =
 	(** Retourne le nouveau point avec les vitesses résultantes de l'algo ORCA*)
-	let dt = 5. in
+	let dt = 3. in
 	let objA = objects.(0) and objB = objects.(1) in
+
+	let v_relat = Geometry.add_subst (-.) objA.speed objB.speed in
 
 	let d_cone = calc_danger_cone objA objB dt in
 
-	let new_v_A = Object.calc_opt_speed objA
-	and new_v_B = Object.calc_opt_speed objB in
-	let v_relat = Geometry.add_subst (-.) new_v_A objB.speed in
+	let v_A_opti = Object.calc_opt_speed objA
+	and v_B_opti = Object.calc_opt_speed objB in
 
 	let in_danger = is_in_danger_zone v_relat d_cone dt in
 
 	render_cone objA objB d_cone in_danger dt;
 
-	if not in_danger then
+	if in_danger then
+		let u = get_correction v_relat d_cone in
+		let s_plan_A = calc_half_plane objA (Geometry.mult_scal 0.5 u) 
+		and s_plan_B = calc_half_plane objB (Geometry.mult_scal (-. 0.5) u) in
+		(***  PROJETER LES V_OPTI SUR LE DEMI PLAN   ***)
+		let pointe_vAopti = (Geometry.add_subst (+.) objA.position v_A_opti) 
+		and pointe_vBopti = (Geometry.add_subst (+.) objB.position v_B_opti) in
+		let point_projected_semiplan_A = Geometry.project_on_plane pointe_vAopti s_plan_A
+		and point_projected_semiplan_B = Geometry.project_on_plane pointe_vBopti s_plan_B in
+		let new_v_A = Geometry.add_subst (-.) point_projected_semiplan_A objA.position 
+		and new_v_B = Geometry.add_subst (-.) point_projected_semiplan_B objB.position in
 		let new_objA = Object.set_obj_speed objA new_v_A
 		and new_objB = Object.set_obj_speed objB new_v_B in
 		[| Object.update_pos new_objA refreshing_time ; Object.update_pos new_objB refreshing_time |]
+
 	else
-		let old_v_relat = Geometry.add_subst (-.) objA.speed objB.speed in
-		let correction = get_correction v_relat d_cone in
-		let (new_objA, new_objB) =  correct objA objB correction in
-		[| Object.update_pos new_objA refreshing_time ; Object.update_pos new_objB refreshing_time |]
-		
+		let u = get_correction v_relat d_cone in
+		let s_plan_A = calc_half_plane objA (Geometry.mult_scal 0.5 u) 
+		and s_plan_B = calc_half_plane objB (Geometry.mult_scal (-. 0.5) u) in
 
+		(***  PROJETER LES V_OPTI SUR LE DEMI PLAN   ***)
+		let pointe_vAopti = (Geometry.add_subst (+.) objA.position v_A_opti) 
+		and pointe_vBopti = (Geometry.add_subst (+.) objB.position v_B_opti) in
+
+		let in_sp = (Geometry.is_in_semi_plan objA.position v_A_opti s_plan_A, Geometry.is_in_semi_plan objB.position v_B_opti s_plan_B) in
+
+		match in_sp with
+		 | (false, false) -> 
+		 	let new_objA = Object.set_obj_speed objA v_A_opti
+			and new_objB = Object.set_obj_speed objB v_B_opti in
+			[| Object.update_pos new_objA refreshing_time ; Object.update_pos new_objB refreshing_time |]
+		 | (true, false) -> 
+		 	let point_projected_semiplan_A = Geometry.project_on_plane pointe_vAopti s_plan_A in
+		 	let new_v_A = Geometry.add_subst (-.) point_projected_semiplan_A objA.position  in
+		 	let new_objA = Object.set_obj_speed objA new_v_A
+			and new_objB = Object.set_obj_speed objB v_B_opti in
+			[| Object.update_pos new_objA refreshing_time ; Object.update_pos new_objB refreshing_time |] 
+		| (false, true) -> 
+		 	let point_projected_semiplan_B = Geometry.project_on_plane pointe_vBopti s_plan_B in
+		 	let new_v_B = Geometry.add_subst (-.) point_projected_semiplan_B objB.position  in
+		 	let new_objA = Object.set_obj_speed objA v_A_opti
+			and new_objB = Object.set_obj_speed objB new_v_B in
+			[| Object.update_pos new_objA refreshing_time ; Object.update_pos new_objB refreshing_time |] 
+		| (true, true) ->
+			let point_projected_semiplan_A = Geometry.project_on_plane pointe_vAopti s_plan_A in
+		 	let new_v_A = Geometry.add_subst (-.) point_projected_semiplan_A objA.position  in
+		 	let new_objA = Object.set_obj_speed objA new_v_A in
+
+			let point_projected_semiplan_B = Geometry.project_on_plane pointe_vBopti s_plan_B in
+		 	let new_v_B = Geometry.add_subst (-.) point_projected_semiplan_B objB.position  in
+			let new_objB = Object.set_obj_speed objB new_v_B in
 
 	
-
-
-(*
-let new_speed objA objB in_danger d_cone =
-	(* S'il n'y a pas de risque de collision alors on calcul leur V_opti
-		et on renvoit  A et B avec des vitesses modifiées *)
-	if not in_danger then
-		let new_v_A = Object.calc_opt_speed objA
-		and new_v_B = Object.calc_opt_speed objB in
-		let new_objA = Object.set_obj_speed objA new_v_A
-		and new_objB = Object.set_obj_speed objB new_v_B in
-		(new_objA, new_objB)
-	else 
-		let vr = Geometry.add_subst (-.) objA.speed objB.speed in
-		let correction = get_correction vr d_cone in
-		correct objA objB correction
-		
-
-
-
-
-
-
-
-
-
-let orca_deux_objet obj set_of_objects =
-	(* Chaque objet a Vitesse Position Rayon de visible et en secret un V optimale *)
-	
-	(* Determiner le cone de risque *)
-
-	(* is_in_cone *)
-
-	(*if is_in_cone vitesse_optimale cone then project vitesse cote_du_cone_le_plus_proche
-	else vitesse_optimale *)
-
-	(* Determiner la vitesse corrigée (la plus proche de vopti) *)
-
-let create_cone dt robot obstacle = 
-	let vect_orientation = add_subst ( - ) obstacle.position robot.position in 
-	let origin_cone_x = robot.position.x +. (obstacle.position.x -. robot.position.x)/. dt in (* on appelle origine le centre du petit cercle ici*)
-	let origin_cone_y = robot.position.y +. (obstacle.position.y -. robot.position.y)/. dt in
-	let origin_cone_vect = {x = origin_cone_x; y = origin_cone_y} in
-	let danger_diameter = (obstacle.diameter + robot.diameter) *. norm (vect_orientation) /. dt in
-	(**** Def du cone de danger ****)
-	{ origin = origin_cone_vect; diameter = danger_diameter; orientation = vect_orientation}
-
-
-let is_in_danger_zone dt robot obstacle = 
-	let d_cone = create_cone dt robot obstacle in 
-	(**** Calcul des composants du cone ****)
-	let vect_orientation = add_subst ( - ) obstacle.position robot.position in 
-	let origin_cone_x = robot.position.x +. (obstacle.position.x -. robot.position.x)/. dt in (* on appelle origine le centre du petit cercle ici*)
-	let origin_cone_y = robot.position.y +. (obstacle.position.y -. robot.position.y)/. dt in
-	let origin_cone_vect = {x = origin_cone_x; y = origin_cone_y} in
-	let danger_diameter = (obstacle.diameter + robot.diameter) *. norm (vect_orientation) /. dt in
-	(**** Def du cone de danger ****)
-	let d_cone = { origin = origin_cone_vect; diameter = danger_diameter; orientation = vect_orientation} in
-	(****Verifier si la pointe du v Vopti est dans le cone****)
-	(*On regarde dans l'ordre angle relatif ? -> avant le cercle ? -> après le cercle ? -> dans le cercle ?*)
-	(*Angle relatif et autres pour les conditions*) 
-	let relat_ang_vrobot_vorient = relative_angle abs_float obstacle.vitesse abs_float vect_orientation in
-	let vnormal = vect_normal_norme vect_orientation *. (diameter/.2) in
-	let pt_tangent = translation_vect_point vect_normal d_cone.origin in
-	let vect_tangent = {x = pt_tangent.x - robot.x ; y = pt_tangent.y - robot.y} in 
-	let relat_ang_vtangent_v_orient = relative_angle vect_tangent vect_orientation in
-	let norm_proj_vrobot_vorient = norm (project robot.speed obstacle.speed) in
-	let vect_origincone_pointevectvitesse = add_subst {x = - robot.speed.x ; y = - robot.speed.y} (mult_scal (1 /. dt) vect_orientation) in
-	(** Debut des conditions **)
-	if relat_ang_vrobot_vorient > relat_ang_vtangent_v_orient then false 
-	else if norm_proj_vrobot_vorient > (norm vect_orientation /. dt) then true
-	else if norm vect_origincone_pointevectvitesse < d_cone.diameter  then true
-	else false
-
-let get_vect_correction dt robot obstacle d_cone=0
-
-*)
